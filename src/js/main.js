fd0d73f67f214fd76ca635de61087df51257c870
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-10-20 16:29:36 -0300

    add intersection observer

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 const openMenuBtn = document.querySelector('#open-menu')
 const closeMenuBtn = document.querySelector('#close-menu')
 const mobileMenu = document.querySelector('#mobile-menu')
+const header = document.querySelector('#header')
+const banner = document.querySelector('#banner')
 
 const openMenu = () => {
     mobileMenu.classList.add('menu-open')
@@ -9,6 +11,24 @@ const openMenu = () => {
 const closeMenu = () => {
     mobileMenu.classList.remove('menu-open')
 }
+
+const observerCallback = (entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            header.classList.add('header-scrolled')
+        } else {
+            header.classList.remove('header-scrolled')
+        }
+    })
+}
+
+const options = {
+    root: null,
+    threshold: 0.5,
+}
+const observer = new IntersectionObserver(observerCallback, options)
+
+observer.observe(banner)
 
 openMenuBtn.addEventListener('click', openMenu)
 closeMenuBtn.addEventListener('click', closeMenu)

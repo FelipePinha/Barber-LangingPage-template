4446d3fede960396bcbac7c5e8799a81f042f668
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-10-22 22:17:10 -0300

    add zap message

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,4 @@
-const openMenuBtn = document.querySelector('#open-menu')
-const closeMenuBtn = document.querySelector('#close-menu')
 const mobileMenu = document.querySelector('#mobile-menu')
-const header = document.querySelector('#header')
 const banner = document.querySelector('#banner')
 
 const openMenu = () => {
@@ -12,7 +9,54 @@ const closeMenu = () => {
     mobileMenu.classList.remove('menu-open')
 }
 
+const clearError = () => {
+    const error = document.querySelector('#error')
+    error.textContent = ''
+}
+
+const handleGetFormValues = () => {
+    const name = document.querySelector('#name').value
+    const message = document.querySelector('#message').value
+
+    if(typeof name !== 'string' || name === '') {
+        const error = document.querySelector('#error')
+        error.textContent = "*Preencha todos os campos"
+        return null
+    }
+
+    if(typeof message !== 'string' || message === '') {
+        const errorSate = document.querySelector('#error')
+        errorSate.textContent = "*Preencha todos os campos"
+        return null
+    }
+
+    return {
+        name,
+        message
+    }
+}
+
+const handleFormSubmit = (event) => {
+    event.preventDefault()
+
+    const formData = handleGetFormValues()
+    
+
+    if(!formData) {
+        return
+    } else {
+        clearError()
+    }
+
+    const phoneNumber = "+55119981428501"
+    const url = `https://wa.me/${phoneNumber}?text=Olá, meu nome é *${formData.name}*.%0a%0a${formData.message}`
+
+    window.open(url, '_blank')
+}
+
 const observerCallback = (entries) => {
+    const header = document.querySelector('#header')
+
     entries.forEach(entry => {
         if(entry.isIntersecting) {
             header.classList.add('header-scrolled')
@@ -22,13 +66,13 @@ const observerCallback = (entries) => {
     })
 }
 
-const options = {
+const observerOptions = {
     root: null,
     threshold: 0.5,
 }
-const observer = new IntersectionObserver(observerCallback, options)
-
+const observer = new IntersectionObserver(observerCallback, observerOptions)
 observer.observe(banner)
 
-openMenuBtn.addEventListener('click', openMenu)
-closeMenuBtn.addEventListener('click', closeMenu)
+document.querySelector('#open-menu').addEventListener('click', openMenu)
+document.querySelector('#close-menu').addEventListener('click', closeMenu)
+document.querySelector('#form').addEventListener('submit', handleFormSubmit)
